84181fe3f7f34a4993cb1602594ccb7a676a5c78
Jose Felix Rodrigues <jose.rodrigues@zmp.co.jp>
2019-08-29 10:25:22 +0900

    Quizz description size

--- js/slickQuiz.js
+++ js/slickQuiz.js
@@ -200,7 +200,7 @@ var finalScore = 0;
 
                 $quizName.hide().html(plugin.config.nameTemplateText
                     .replace('%name', quizValues.info.name) ).fadeIn(1000, kN(key,1));
-                $quizHeader.hide().prepend($('<div class="quizDescription">' + quizValues.info.main + '</div>')).fadeIn(1000, kN(key,2));
+                $quizHeader.hide().prepend($('<div class="h4 quizDescription">' + quizValues.info.main + '</div>')).fadeIn(1000, kN(key,2));
                 $quizResultsCopy.append(quizValues.info.results);
 
                 // add retry button to results view, if enabled
